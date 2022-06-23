c3fccd0e12ca504a8d7011c0411554ee934e14d0
DaMinor.CKB <103505804+DaMinor336@users.noreply.github.com>
2022-06-23 09:48:59 +0300

    Update Интеграция с парусом.sql

--- SQL/Интеграция с парусом.sql
+++ SQL/Интеграция с парусом.sql
@@ -5,13 +5,13 @@ select *
 from z.ckb_import_logs
 where process='Импорт заместителей' and
       action='Ошибка' and
-      crt_date>to_date('01.04.2022 13:00','dd.mm.yyyy hh24:mi')
+      crt_date>to_date('01.05.2022 13:00','dd.mm.yyyy hh24:mi')
 order by id
 --
 -- Отсутствуют даты заврешения замещений
 select *
 from usr_prordersp_zamest_ar@parus
-where dwork>to_date('01.12.2021','dd.mm.yyyy') and
+where dwork>to_date('01.05.2022','dd.mm.yyyy') and
       dbend_for is null and
       dbend_for_alt is null and
       dbend_for_alt2 is null
