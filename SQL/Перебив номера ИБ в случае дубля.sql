f0404bf1ada143268ff675aec73ec75043977537
DaMinor.CKB <103505804+DaMinor336@users.noreply.github.com>
2022-06-27 10:10:15 +0300

    hui
    l,l;p,

--- SQL/Перебив номера ИБ в случае дубля.sql
+++ SQL/Перебив номера ИБ в случае дубля.sql
@@ -2,16 +2,23 @@ declare
   ak_num number;
 begin
     ak_num     := t_chrt_codes.get_numb_next(
-      'ИБ', 
+      'ИБ',
       1,
       99999999,
       'ANY',
       'FIRST_FREE',
-      'I_MC');
+      'I_MC'); -- Для круглостуточного стационара.
+					     -- Для дневного и третьего стационара а также для амбулаторных
+               -- необходимо использовать другую компоненту
 
     t_chrt_codes.lock_chart_number(
       ak_num,
       'ИБ',
       'I_MC');
-  dbms_output.put_line('ib_num= '||ak_num);
+
+	 update t_med_chrts m
+		 set m.code = ak_num
+	where m.id = ??????? --ID ИБ
+
+  dbms_output.put_line('ib_num= '||ak_num); -- Строка для отладки
 end;
